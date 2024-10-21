6a84497f1fcc8a406ee8d56ff758f47aeedb82a3
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2024-10-21 17:46:31 +0530

    Hero

--- 01_21Oct2024/src/Components/Hero.jsx
+++ 01_21Oct2024/src/Components/Hero.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function Hero() {
   return (
     <>
-        <div className='w-screen flex h-screen'>
-            <div className='bg-[#121212] w- '></div>
-            <div className='bg-gray-500 w-'></div>
+      <div className="w-screen flex h-screen">
+        <div className="bg-[#121212] w-[40%]"></div>
+        <div className='w-[60%] bg-[url("/heroBg.jpeg")] bg-cover bg-center'>
+          You can add content here if needed
         </div>
-      
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
