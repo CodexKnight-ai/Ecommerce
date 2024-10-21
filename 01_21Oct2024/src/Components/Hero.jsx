2fba70caff635f0078539a756671e4ef3a821d50
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2024-10-21 17:34:43 +0530

    First day:Hero and Footer

---
+++ 01_21Oct2024/src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+        <div className='w-screen flex h-screen'>
+            <div className='bg-[#121212] w- '></div>
+            <div className='bg-gray-500 w-'></div>
+        </div>
+      
+    </>
+  )
+}
+
+export default Hero
